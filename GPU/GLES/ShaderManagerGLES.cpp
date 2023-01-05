244fdc435dadc77fad8b1b755f67cc5eca67c8fd
Henrik Rydgård <hrydgard@gmail.com>
2023-01-05 15:37:42 +0100

    Vulkan: Don't use non-indexed draws for pure tristrips and fans, creates extra pipelines.
    Bump shader cache version

--- GPU/GLES/ShaderManagerGLES.cpp
+++ GPU/GLES/ShaderManagerGLES.cpp
@@ -937,7 +937,7 @@ std::string ShaderManagerGLES::DebugGetShaderString(std::string id, DebugShaderT
 // as sometimes these features might have an effect on the ID bits.
 
 #define CACHE_HEADER_MAGIC 0x83277592
-#define CACHE_VERSION 20
+#define CACHE_VERSION 21
 struct CacheHeader {
 	uint32_t magic;
 	uint32_t version;
